abc6194ae8b1b7e8b137def70a9732a0c489999b
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-21 11:29:30 +0800

    Refresh PWA cache for exact workcat icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='meow-work-pwa-v8';
-const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon-180.png','./app-icon-512.jpg'];
+const CACHE_NAME='meow-work-pwa-v7';
+const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest?v=7','./app-icon-180.png?v=7','./app-icon-512.jpg?v=7'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()));
