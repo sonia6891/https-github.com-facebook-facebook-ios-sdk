6dc9328b2b2081d707723b7d561425bae397ed10
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-21 15:26:35 +0800

    Refresh PWA cache for salary quote title

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='meow-work-pwa-v22';
-const STATIC_ASSETS=['./manifest.webmanifest?v=22'];
+const CACHE_NAME='meow-work-pwa-v23';
+const STATIC_ASSETS=['./manifest.webmanifest?v=23'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
