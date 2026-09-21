aa8f6895459c93024cae67bad082b93daaafb870
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-21 10:09:44 +0800

    Refresh PWA cache after full UI rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='shift-pay-pwa-v3';
+const CACHE_NAME='meow-work-pwa-v4';
 const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
