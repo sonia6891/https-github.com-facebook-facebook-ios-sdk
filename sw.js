28fd5cb43ee32917b4db7236c3b730e29c3d644e
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-21 09:00:29 +0800

    Bump PWA cache for latest header and branding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='shift-pay-pwa-v1';
+const CACHE_NAME='shift-pay-pwa-v2';
 const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
