8bd748b8378813025b9bd9c62a1a26a62111d261
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 06:32:58 +0800

    Refresh PWA cache to v51

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='meow-work-pwa-v50';
-const STATIC_ASSETS=['./manifest.webmanifest?v=50'];
+const CACHE_NAME='meow-work-pwa-v51';
+const STATIC_ASSETS=['./manifest.webmanifest?v=51'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
