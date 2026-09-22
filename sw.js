5b5b6de2530a28022de95fe1d27d1d025a75dead
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-23 05:04:49 +0800

    Remove duplicate settings cards

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v109';
-const STATIC_ASSETS=['./manifest.webmanifest?v=109'];
+const CACHE_NAME='meow-work-pwa-v110';
+const STATIC_ASSETS=['./manifest.webmanifest?v=110'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
