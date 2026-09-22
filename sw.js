9f6f0a6ea0e319189048fd0220ebf8caf2f8c72b
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 08:27:29 +0800

    Refresh PWA cache for OCR v2 v57

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='meow-work-pwa-v56';
-const STATIC_ASSETS=['./manifest.webmanifest?v=56'];
+const CACHE_NAME='meow-work-pwa-v57';
+const STATIC_ASSETS=['./manifest.webmanifest?v=57'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
