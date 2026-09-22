275d448e5a67915c23b86a039a6d3e9d27a25e20
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 18:25:25 +0000

    Separate manual and calendar overtime sources (v101)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v100';
-const STATIC_ASSETS=['./manifest.webmanifest?v=100'];
+const CACHE_NAME='meow-work-pwa-v101';
+const STATIC_ASSETS=['./manifest.webmanifest?v=101'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
