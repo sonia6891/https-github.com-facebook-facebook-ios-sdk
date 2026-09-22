7e2c410345f09d96940fe8636f59a6c7d512ddce
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 17:21:13 +0000

    Allow schedule changes only while settings are expanded (v95)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v94';
-const STATIC_ASSETS=['./manifest.webmanifest?v=94'];
+const CACHE_NAME='meow-work-pwa-v95';
+const STATIC_ASSETS=['./manifest.webmanifest?v=95'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
