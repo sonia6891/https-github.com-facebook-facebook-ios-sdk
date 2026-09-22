78ada7fe114eb96cc9e8a2c2331f3e4106bfad01
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 10:17:50 +0800

    Bump PWA cache to v65

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v64';
-const STATIC_ASSETS=['./manifest.webmanifest?v=64'];
+const CACHE_NAME='meow-work-pwa-v65';
+const STATIC_ASSETS=['./manifest.webmanifest?v=65'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
