83b540680f59d528818d16f7d7163cbbb01d9dc4
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-23 04:59:33 +0800

    Prevent mobile hire date control overflow

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v108';
-const STATIC_ASSETS=['./manifest.webmanifest?v=108'];
+const CACHE_NAME='meow-work-pwa-v109';
+const STATIC_ASSETS=['./manifest.webmanifest?v=109'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
