7409fc28d8dfeb6f9d628fd7389fe513373186b5
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 09:43:24 +0800

    Refresh service worker to v62

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME='meow-work-pwa-v61';
-const STATIC_ASSETS=['./manifest.webmanifest?v=61'];
+self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
+const CACHE_NAME='meow-work-pwa-v62';
+const STATIC_ASSETS=['./manifest.webmanifest?v=62'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
