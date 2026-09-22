8e506cf1c404ede66bf76501ec2915f8514dac21
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 18:03:40 +0800

    Unify dashboard section headings with paw marker (v79)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v78';
-const STATIC_ASSETS=['./manifest.webmanifest?v=78'];
+const CACHE_NAME='meow-work-pwa-v79';
+const STATIC_ASSETS=['./manifest.webmanifest?v=79'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
