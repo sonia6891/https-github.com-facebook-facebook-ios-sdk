c59a489d78c2df834be9d764ae410e8ec74b3479
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-22 15:48:37 +0000

    Add configurable four-team three-shift scheduling mode (v91)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
-const CACHE_NAME='meow-work-pwa-v90';
-const STATIC_ASSETS=['./manifest.webmanifest?v=90'];
+const CACHE_NAME='meow-work-pwa-v91';
+const STATIC_ASSETS=['./manifest.webmanifest?v=91'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
