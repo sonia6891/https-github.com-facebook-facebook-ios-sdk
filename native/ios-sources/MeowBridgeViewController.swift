ec3cb25170a45bfd58815631ddb90bc1705ef3cd
sonia6891 <107385898+sonia6891@users.noreply.github.com>
2026-09-23 21:10:24 +0800

    Add Capacitor bridge controller for StoreKit plugin

---
+++ native/ios-sources/MeowBridgeViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Capacitor
+
+final class MeowBridgeViewController: CAPBridgeViewController {
+    override open func capacitorDidLoad() {
+        bridge?.registerPluginInstance(MeowStoreBillingPlugin())
+    }
+}
